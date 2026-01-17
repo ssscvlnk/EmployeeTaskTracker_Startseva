eb4c76be93084b7c84f2ad9f743403fa8a0fe5cb
ssscvlnk <pstartceva@yandex.ru>
2026-01-17 19:11:09 +0300

    Implement custom setters for salary and experience

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,6 +1,30 @@
 class Employee {
-    private val fullname: String = ""
-    private val position: String = ""
-    private val salary: Int = 0
-    private val yearsOfExperience: Int = 0
+    var fullname: String= ""
+        get
+    var position: String = ""
+        get
+    var salary: Int = 0
+        get
+        set(value) {
+            if (value<0) println("Зарплата не может быть отрицательной")
+            else field = value
+        }
+    var yearsOfExperience: Int = 0
+        get
+        set(value) {
+            if (value>50) {
+                println("Опыт работы не может быть больше 50 лет")
+                field = 50
+            } else field = value
+        }
+}
+
+fun main() {
+    val employee = Employee()
+    employee.fullname = "Старцева Полина Сергеевна"
+    employee.position = "Программист"
+    employee.salary = -1000
+    employee.yearsOfExperience = 55
+    employee.salary = 100000
+    employee.yearsOfExperience  = 2
 }
