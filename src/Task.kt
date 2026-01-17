2acd415d204b5eeb47aaf400933ff06d021b1632
ssscvlnk <pstartceva@yandex.ru>
2026-01-17 19:36:40 +0300

    Demonstrate equals, copy and toString for Task

--- src/Task.kt
+++ src/Task.kt
@@ -10,3 +10,24 @@ data class Task(
     val isCompleted: Boolean = false
 )
 
+fun main() {
+    val task1 = Task(
+        "Сделать ДЗ",
+        "Задание по РМП",
+        priority = Priority.Medium
+    )
+    val task2 = Task(
+        "Пройти курс на Stepik",
+        "Курс С# для продвинутых",
+        priority = Priority.Low
+    )
+    val task3 = Task(
+        "Создать проект",
+        "Написать код для собственного приложения",
+        priority = Priority.Low
+    )
+    println("task1 == task2: ${task1==task2}")
+    println(task1)
+    val task4 = task1.copy(priority = Priority.High)
+    println("Копия task1 с другим приоритетом: $task4")
+}
