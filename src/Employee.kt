f7f0f326ebce30e7c4c51d95de129f2528479db0
ssscvlnk <pstartceva@yandex.ru>
2026-01-18 10:59:17 +0300

    Implement assignTask method with validation

--- src/Employee.kt
+++ src/Employee.kt
@@ -17,7 +17,16 @@ class Employee : ReportGenerator {
                 field = 50
             } else field = value
         }
-
+    var currentTask: Task? = null
+        get
+    fun assignTask(newTask: Task) {
+        if (currentTask != null && currentTask?.isCompleted == false) {
+            println("Сотрудник уже занят задачей ${currentTask?.title}")
+        } else {
+            currentTask = newTask
+            println("Задача '${currentTask?.title}' назначена сотруднику $fullname")
+        }
+    }
     override fun generateReport(): String {
         return "Имя сотрудника: $fullname, должность: $position, зарплата: $salary, опыт работы: $yearsOfExperience"
     }
@@ -27,8 +36,26 @@ fun main() {
     val employee = Employee()
     employee.fullname = "Старцева Полина Сергеевна"
     employee.position = "Программист"
-    employee.salary = -1000
-    employee.yearsOfExperience = 55
+//    employee.salary = -1000
+//    employee.yearsOfExperience = 55
     employee.salary = 100000
     employee.yearsOfExperience  = 2
+    val developmentDepartment = DevelopmentDepartment("Отдел разработки")
+    val task1 = Task(
+        "Создать проект",
+        "Написать код для собственного приложения",
+        priority = Priority.Low
+    )
+    val task2 = Task(
+        "Изучить новый материал",
+        "Научиться писать код HTML",
+        priority = Priority.Medium
+    )
+    employee.assignTask(task2)
+    employee.assignTask(task1)
+    val list = listOf<ReportGenerator>(employee, developmentDepartment)
+    println("Сводный отчет: ")
+    for (report in list) {
+        println(report.generateReport())
+    }
 }
